4dbe9fe715a215d739b5638600bdbdde7c543d96
Muhammad <muhammad.safarov@web.de>
2021-05-22 14:44:25 +0200

    Added a game Map  with TILED

--- scenes/Play.js
+++ scenes/Play.js
@@ -4,11 +4,15 @@ class Play extends Phaser.Scene {
   constructor() {
     super("PlayScene");
   }
-  preload() {
-    this.load.image("sky", "assets/sky.png");
-  }
+
   create() {
-    this.add.image(0, 0, "sky").setOrigin(0);
+    const map = this.make.tilemap({ key: "map" });
+    const teilset1 = map.addTilesetImage("main_lev_build_1", "tiles-1");
+    // const teilset2 = map.addTilesetImage("main_lev_build_2", "tiles-2");
+
+    //ORDER MATTERS
+    map.createStaticLayer("environment", teilset1);
+    map.createStaticLayer("platforms", teilset1);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import PlayScene from "../scenes/Play";
+import PreloadScene from "../scenes/Preload";
 
 const WIDTH = 1200;
 const HEIGHT = 600;
@@ -9,7 +10,7 @@ const SHARED_CONFIG = {
   height: HEIGHT
 };
 
-const Scenes = [PlayScene];
+const Scenes = [PreloadScene, PlayScene];
 const createScene = Scene => new Scene(SHARED_CONFIG);
 const initScenes = () => Scenes.map(createScene);
 
